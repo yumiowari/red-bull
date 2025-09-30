bef9cdc4d3d439390e002bf7e6cac137be512975
Yumiowari <owariyumi@gmail.com>
2025-09-30 16:20:14 -0300

    Nova resolução da imagem e compressão JPEG

--- script.py
+++ script.py
@@ -1,18 +1,25 @@
 import cv2
 import os
 
-def process_video(path):
+def process_video(path, width, height):
     video = cv2.VideoCapture(path)
 
-    # Cria pasta de saída com o nome do vídeo (sem extensão)
+    # cria a pasta de saída com o nome do vídeo (sem extensão)
+    #file = os.path.basename(path)
+    #out = os.path.splitext(file)[0]
     out = os.path.splitext(path)[0]
     os.makedirs(out, exist_ok=True)
 
     count, success = 0, True
     while success:
-        success, image = video.read() # Lê o frame
-        if success: 
-            cv2.imwrite(f'{out}/frame{count}.jpg', image) # Salva o frame
+        # lê o frame
+        success, image = video.read()
+        if success:
+            # redimensiona a imagem para a resolução desejada
+            resized = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+        
+            # salva o frame
+            cv2.imwrite(f'{out}/frame{count}.jpg', image, [cv2.IMWRITE_JPEG_QUALITY, 70]) # <--- 70% de compressão JPEG
             count += 1
 
     video.release()
@@ -26,4 +33,4 @@ if __name__ == '__main__':
         if path == 'quit':
             break;
         else:
-            process_video(f'./src/{path}')
+            process_video(f'./src/{path}', 360, 640) # proporção 9:16 (youtube shorts)
